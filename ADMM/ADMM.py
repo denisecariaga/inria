fa70e7937570c6fcb8f97d3fcd10f40ff4f62102
Denise Cariaga Sandoval <dcariaga@inrialpes.fr>
2020-02-11 17:22:53 +0100

    projection updated

--- ADMM/ADMM.py
+++ ADMM/ADMM.py
@@ -105,15 +105,15 @@ class ADMM:
             x1 = vector_per_contact[i][0]
             normx2 = np.linalg.norm(vector_per_contact[i][1:])
 
-            if normx2 <= (-mui) * x1:
+            if normx2 <= (-1/mui) * x1:
                 projected = np.concatenate((projected, np.zeros([self.dim1, ])))
-            elif normx2 <= (1 / mui) * x1:
+            elif normx2 <= mui * x1:
                 projected = np.concatenate((projected, vector_per_contact[i]))
             else:
                 x2 = vector_per_contact[i][1:]
                 projected = np.concatenate((projected,
-                                            (mui ** 2) / (1 + mui ** 2) * (x1 + (1 / mui) * normx2) * np.concatenate(
-                                                (np.array([1]), (1 / mui) * x2 * (1 / normx2)))))
+                                            (1/ (1 + mui ** 2)) * (x1 + mui * normx2) * np.concatenate(
+                                                (np.array([1]), mui * x2 * (1 / normx2)))))
 
         return projected
 
