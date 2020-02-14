edc222e1379510c5f6086b2f6bd301cdb2f5f3cd
DENISE CARIAGA <dccariaga@uc.cl>
2020-02-14 19:00:26 +0100

    stop criteria added

--- ADMM/ADMM.py
+++ ADMM/ADMM.py
@@ -36,117 +36,133 @@ from ADMM.Solver.Tolerance.iter_tolerance import *
 
 
 class ADMM:
-    def __init__(self, problem_data, rho_method):
-        #EXTRACTING PROBLEM INFORMATION
-        self.pd = problem_data()
-        self.rho_method = rho_method
+	def __init__(self, problem_data, rho_method):
+		# EXTRACTING PROBLEM INFORMATION
+		self.pd = problem_data()
+		self.rho_method = rho_method
 
-        #TIMES
-        self.start = time.clock()
-        self.end = time.clock()
+		# TIMES
+		self.start = time.clock()
+		self.end = time.clock()
 
-        #RESIDUALS
-        self.r_prim = self.pd.r_prim
-        self.r_dual = self.pd.r_dual
+		# RESIDUALS
+		self.r_prim = self.pd.r_prim
+		self.r_dual = self.pd.r_dual
+		self.r_prim_norm = self.pd.r_prim_norm
+		self.r_dual_norm = self.pd.r_dual_norm
 
-        #TITLE
-        self.title = self.pd.title
+		# TITLE
+		self.title = self.pd.title
 
-        #KNOWN PARAMETERS
-        self.mu = self.pd.mu
-        self.M = self.pd.M
-        self.f = self.pd.f
-        self.w = self.pd.w
-        self.W = self.pd.W
-        self.q = self.pd.q
-        self.Id = self.pd.Id
+		# KNOWN PARAMETERS
+		self.mu = self.pd.mu
+		self.M = self.pd.M
+		self.f = self.pd.f
+		self.w = self.pd.w
+		self.W = self.pd.W
+		self.q = self.pd.q
+		self.Id = self.pd.Id
 
-        #PROBLEM DIMENSIONS
-        self.dim1 = 3
-        self.m = self.pd.m
-        self.n = self.pd.n
-        self.n_c = self.m / self.dim1
+		# PROBLEM DIMENSIONS
+		self.dim1 = 3
+		self.m = self.pd.m
+		self.n = self.pd.n
+		self.n_c = self.m / self.dim1
 
-        #UNKNOWN VARIABLES
-        self.u = self.pd.u
-        self.r = self.pd.r
-        self.p = self.pd.p
-        self.s = self.pd.s
-        self.x = self.pd.x
+		# UNKNOWN VARIABLES
+		self.u = self.pd.u
+		self.r = self.pd.r
+		self.p = self.pd.p
+		self.s = self.pd.s
+		self.x = self.pd.x
 
-        #OTHERS
-        A = self.W + self.rho_method * csc_matrix(self.Id)
-        self.LU = linalg.splu(A)
+		# OTHERS
+		A = self.W + self.rho_method * csc_matrix(self.Id)
+		self.LU = linalg.splu(A)
 
-    def plot(self):
-        R = [np.linalg.norm(k) for k in self.r_prim]
-        S = [np.linalg.norm(k) for k in self.r_dual]
-        plt.semilogy(R, label='||r||')
-        plt.hold(True)
-        plt.semilogy(S, label='||s||')
-        plt.hold(True)
-        plt.ylabel('Residuals')
-        plt.xlabel('Iteration')
-        plt.text(len(self.r_prim) / 2, np.log(np.amax(S) + np.amax(R)) / 10, 'N_iter = ' + str(len(self.r_prim) - 1))
-        plt.text(len(self.r_prim) / 2, np.log(np.amax(S) + np.amax(R)) / 100,
-                 'Total time = ' + str((self.end - self.start) * 10 ** 3) + ' ms')
-        plt.text(len(self.r_prim) / 2, np.log(np.amax(S) + np.amax(R)) / 1000,
-                 'Time_per_iter = ' + str(((self.end - self.start) / (len(self.r_prim) - 1)) * 10 ** 3) + ' ms')
-        plt.title(self.title)
-        plt.legend()
-        plt.show()
+	def plot(self):
+		R = [np.linalg.norm(k) for k in self.r_prim]
+		S = [np.linalg.norm(k) for k in self.r_dual]
+		plt.semilogy(R, label='||r||')
+		plt.hold(True)
+		plt.semilogy(S, label='||s||')
+		plt.hold(True)
+		plt.ylabel('Residuals')
+		plt.xlabel('Iteration')
+		plt.text(len(self.r_prim) / 2, np.log(np.amax(S) + np.amax(R)) / 10, 'N_iter = ' + str(len(self.r_prim) - 1))
+		plt.text(len(self.r_prim) / 2, np.log(np.amax(S) + np.amax(R)) / 100,
+				 'Total time = ' + str((self.end - self.start) * 10 ** 3) + ' ms')
+		plt.text(len(self.r_prim) / 2, np.log(np.amax(S) + np.amax(R)) / 1000,
+				 'Time_per_iter = ' + str(((self.end - self.start) / (len(self.r_prim) - 1)) * 10 ** 3) + ' ms')
+		plt.title(self.title)
+		plt.legend()
+		plt.show()
 
-    def projection(self, vector):
-        vector_per_contact = np.split(vector, self.n_c)
-        projected = np.array([])
+	def projection(self, vector):
+		vector_per_contact = np.split(vector, self.n_c)
+		projected = np.array([])
 
-        for i in range(int(self.n_c)):
-            mui = self.mu[i]
-            x1 = vector_per_contact[i][0]
-            normx2 = np.linalg.norm(vector_per_contact[i][1:])
+		for i in range(int(self.n_c)):
+			mui = self.mu[i]
+			x1 = vector_per_contact[i][0]
+			normx2 = np.linalg.norm(vector_per_contact[i][1:])
 
-            if normx2 <= (-1/mui) * x1:
-                projected = np.concatenate((projected, np.zeros([self.dim1, ])))
-            elif normx2 <= mui * x1:
-                projected = np.concatenate((projected, vector_per_contact[i]))
-            else:
-                x2 = vector_per_contact[i][1:]
-                projected = np.concatenate((projected,
-                                            (1/ (1 + mui ** 2)) * (x1 + mui * normx2) * np.concatenate(
-                                                (np.array([1]), mui * x2 * (1 / normx2)))))
+			if normx2 <= (-1/mui) * x1:
+				projected = np.concatenate((projected, np.zeros([self.dim1, ])))
+			elif normx2 <= mui * x1:
+				projected = np.concatenate((projected, vector_per_contact[i]))
+			else:
+				x2 = vector_per_contact[i][1:]
+				projected = np.concatenate((projected,
+											(1/ (1 + mui ** 2)) * (x1 + mui * normx2) * np.concatenate(
+												(np.array([1]), mui * x2 * (1 / normx2)))))
 
-        return projected
+		return projected
 
-    def Gs_matrix(self):
-        E_ = np.array([])
+	def Gs_Matrix(self):
+		E_ = np.array([])
 
-        u_per_contact = np.split(self.u, self.n_c)
+		u_per_contact = np.split(self.u, self.n_c)
 
-        for i in range(self.n_c):
-            E_ = np.concatenate(E_, np.array([1, 0, 0]) * self.mu[i] * np.linalg.norm(u_per_contact[i][1:]))
-        E = E_[:, np.newaxis]
+		for i in range(self.n_c):
+			E_ = np.concatenate(E_, np.array([1, 0, 0]) * self.mu[i] * np.linalg.norm(u_per_contact[i][1:]))
+		E = E_[:, np.newaxis]
 
-        return np.squeeze(E)
+		return np.squeeze(E)
 
-    def r_update(self, k):
-        RHS = -self.q + self.s + self.rho_method * csc_matrix(self.p[k] - self.x[k])
-        self.r.append(self.LU.solve(RHS))
-        return self.r
+	def r_update(self, k):
+		RHS = -self.q + self.s + self.rho_method * csc_matrix(self.p[k] - self.x[k])
+		self.r.append(self.LU.solve(RHS))
+		return self.r
 
-    def p_update(self, k):
-        vector = self.r[k] + self.x[k]
-        self.p.append(self.projection(vector))
-        return self.p
+	def p_update(self, k):
+		vector = self.r[k] + self.x[k]
+		self.p.append(self.projection(vector))
+		return self.p
 
-    def x_update(self, k):
-        self.x.append(self.x[k] + self.r[k+1] - self.p[k+1])
-        return self.x
+	def x_update(self, k):
+		self.x.append(self.x[k] + self.r[k+1] - self.p[k+1])
+		return self.x
 
-    def residuals_update(self, k):
-        self.r_prim.append(self.r[k+1] - self.p[k+1])
-        self.r_dual.append(self.rho_method * csc_matrix.dot(self.Id, (self.p[k] - self.p[k+1])))
+	def residuals_update(self, k):
+		self.r_prim.append(self.r[k+1] - self.p[k+1])
+		self.r_dual.append(self.rho_method * csc_matrix.dot(self.Id, (self.p[k] - self.p[k+1])))
 
-    def stop_criteria(self):
+	def stop_criteria(self, k):
+		eps_max_prim = np.amax(np.array([np.linalg.norm(csr_matrix.dot(self.Id, self.r[k+1])),np.linalg.norm(csr_matrix.dot(- self.Id, self.p[k+1])), 0))
+		eps_pri = np.sqrt(p)*ABSTOL + RELTOL*eps_max_prim
+
+		eps_max_dual = np.linalg.norm(self.rho * csc_matrix.dot(self.Id, xi[k + 1]))
+		eps_dual = np.sqrt(self.n) * ABSTOL + RELTOL * eps_max_dual
+
+		self.r_prim_norm.append(np.linalg.norm(self.r_prim[k + 1]))
+		self.r_dual_norm.append(np.linalg.norm(self.r_dual[k + 1]))
+
+Este criterio va en el codigo cuando use esta clase
+
+		if self.r_prim_norm[k + 1] <= eps_pri and self.r_dual_norm[k + 1] <= eps_dual:
+			#break
+
 
 
 
--- ADMM/data.py
+++ ADMM/data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_matrix, linalg
 import math
+import time
 
 
 # n = 5
@@ -10,26 +11,26 @@ import math
 class ProblemData:
     def __init__(self):
         # m sized variables
-        self.r = [np.zeros((6,))]
-        self.p = [np.zeros((6,))]
-        self.s = [np.zeros((6,))]
-        self.x = [np.zeros((6,))]
+        self.r = np.zeros((6,1))
+        self.p = np.zeros((6,1))
+        self.s = np.zeros((6,1))
+        self.x = np.zeros((6,1))
 
         # mxn sized matrix
         self.H = np.ones((6, 5))
 
         # nxn sized matrix
-        self.M = np.ones((5, 5))
+        self.M = np.arange(1,26).reshape(5,5)
 
         # n sized parameters
-        self.f = [np.ones((5,))]
+        self.f = np.ones((5,1))
 
         # mxm sized matrix
-        self.Id = [np.identity(6)]
+        self.Id = np.identity(6)
         self.W = (self.H.dot(np.linalg.inv(self.M))).dot(self.H.transpose())
 
         # m sized parameters
-        self.w = np.ones((6,))
+        self.w = np.ones((6,1))
         self.q = - (self.H.dot(np.linalg.inv(self.M))).dot(self.f)
         self.u = self.W.dot(self.r) + self.q
         self.u_hat = self.u + self.s
@@ -38,7 +39,8 @@ class ProblemData:
         self.mu = np.random.uniform(0, 1, 2)
 
     def rho_ghadimi(self):
-        DUAL = csr_matrix.dot(csr_matrix.dot(self.H, linalg.inv(self.M)), self.H.transpose())
+        DUAL = csr_matrix.dot(csr_matrix.dot(csr_matrix.self.H, linalg.inv(self.M)),
+                              self.H.transpose())
         eig, eig_vect = linalg.eigs(DUAL)  # sparse
 
         eigmax = np.absolute(np.amax(eig))
@@ -61,12 +63,10 @@ class ProblemData:
         return 1
 
 
-
-
-
-
-
-
+start = time.clock()
 p = ProblemData()
-print(p.f[0][3])
+print(p.f[3])
+print(type(p.u))
 print(np.shape(p.w)[0])
+end = time.clock()
+print(end-start)
